4c160b3e8263949314f765f4200c257e9c58cbdc
Amit Paudel <amitpaudel789@gmail.com>
2025-11-10 13:15:23 +0545

    navbar ui

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { HiBars3CenterLeft } from 'react-icons/hi2';
+import { IoMdSearch } from 'react-icons/io';
+import { HiOutlineUser } from 'react-icons/hi';
+import { HiOutlineHeart } from 'react-icons/hi';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { NavLink } from 'react-router-dom';
+import avatarImg from '../assets/avatar.png';
+const navigations = [
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Orders', href: '/orders' },
+  {
+    name: 'Cart Page',
+    href: '/cartpage',
+  },
+  { name: 'Check Out', href: '/checkout' },
+];
+
+const Navbar = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const currentUser = true;
+
+  return (
+    <header className="max-w-screen-2xl  mx-auto px-4 py-6 ">
+      <nav className="flex justify-between items-center">
+        {/* Left Side */}
+        <div className="flex items-center md:gap-16 gap-4">
+          {/* Hamburger */}
+
+          <NavLink to="/">
+            <HiBars3CenterLeft className="size-6" />
+          </NavLink>
+
+          {/* Search Input */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoMdSearch className="absolute inline-block left-3 inset-y-2" />
+
+            <input
+              type="text"
+              placeholder="Search-here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none "
+            />
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <>
+                <button onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
+                  <img
+                    src={avatarImg}
+                    alt=""
+                    className={`size-7 rounded-full ${
+                      currentUser ? 'ring-2 ring-blue-500' : ''
+                    }`}
+                  />
+                </button>
+
+                {isDropDownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40 ">
+                    <ul className="py-2 ">
+                      {navigations.map((item) => {
+                        return (
+                          <li
+                            key={item.name}
+                            onClick={() => setIsDropDownOpen(false)}
+                          >
+                            <NavLink
+                              to={item.href}
+                              className="block px-4 py-2 text-sm hover:bg-gray-100"
+                            >
+                              {item.name}
+                            </NavLink>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <NavLink to="/login">
+                <HiOutlineUser className="size-6" />
+              </NavLink>
+            )}
+          </div>
+
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6 " />
+          </button>
+
+          <NavLink
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <HiOutlineShoppingCart className="" />
+            <span className="text-sm font-semibold sm:ml-1 ">0</span>
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>Navbar</nav>
-      <main>
+      <Navbar />
+      <main className="min-h-screen min-w-screen-2xl mx-auto px-4 py-6 font-nunito">
         <Outlet></Outlet>
       </main>
       <footer>Footer</footer>
