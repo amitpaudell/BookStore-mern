4b2e8cf2dd8feac01b0668164b0280f37792ef38
Amit Paudel <amitpaudel789@gmail.com>
2025-11-17 22:08:24 +0545

    routing for order changes

---
+++ backend/src/orders/order.controller.js
@@ -0,0 +1,20 @@
+const Order = require('./order.model');
+
+exports.createOrder = async (req, res, next) => {
+  try {
+    const newOrder = await Order(req.body);
+    const saveOrder = await newOrder.save();
+    res.status(200).json({ message: 'Order place sucessfully' }, saveOrder);
+  } catch (error) {
+    console.error('Error while creating order', error);
+    res.status(500).json({ message: 'Failed to create order' });
+  }
+};
+
+exports.getOrderByEmail = async (req, res, next) => {
+  try {
+    const 
+  } catch (error) {
+
+  }
+};
