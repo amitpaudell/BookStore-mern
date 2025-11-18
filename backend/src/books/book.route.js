919760cc4ac79c5882d947f19f786453fa639cfd
Amit Paudel <amitpaudel789@gmail.com>
2025-11-18 17:45:10 +0545

    JWT Token

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const bookController = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 
-router.post('/create-book', bookController.createBook);
+router.post('/create-book', verifyAdminToken, bookController.createBook);
 
 router.get('/', bookController.getBooks);
 
