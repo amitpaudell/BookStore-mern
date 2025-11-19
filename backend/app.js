33401c27004c7cf19fc2665b4196697d45d07c06
Amit Paudel <amitpaudel789@gmail.com>
2025-11-19 23:20:25 +0545

    change

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://book-store-mern-pearl.vercel.app/',
+      'https://book-store-mern-pearl.vercel.app',
     ],
     credentials: true,
   })
