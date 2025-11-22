d4fd7424f168fb25f209ceb6cb6faa07d9239c14
Amit Paudel <amitpaudel789@gmail.com>
2025-11-22 22:05:27 +0545

    update

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -4,14 +4,19 @@ const router = express.Router();
 const bookController = require('./book.controller');
 const verifyAdminToken = require('../middleware/verifyAdminToken');
 
+//create a book
 router.post('/create-book', verifyAdminToken, bookController.createBook);
 
+//get all books
 router.get('/', bookController.getBooks);
 
+//get single book
 router.get('/:id', bookController.getBook);
 
+//update a book
 router.put('/edit/:id', verifyAdminToken, bookController.updateBook);
 
+//delete a book
 router.delete('/:id', verifyAdminToken, bookController.deleteBook);
 
 module.exports = router;
